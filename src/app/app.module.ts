6b7cd2d55cb7600f4322803bd379f9b4b4d6a353
royeradames <royeraadames@gmail.com>
2021-11-23 16:48:39 -0500

    📦declare all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CrewComponent } from './crew/crew.component';
+import { DestinationComponent } from './destination/destination.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { TechnologyComponent } from './technology/technology.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrewComponent,
+    DestinationComponent,
+    HeaderComponent,
+    HomeComponent,
+    TechnologyComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
