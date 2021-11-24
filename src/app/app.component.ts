6879efcb127786f286aca782f20a7f3bbddbe80b
royeradames <royeraadames@gmail.com>
2021-11-23 20:32:32 -0500

    📦init app ts component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'space-tourism-website';
+  constructor() {}
 }
