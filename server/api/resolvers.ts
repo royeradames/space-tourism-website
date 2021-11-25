bc5459de498778ffcfb55722f1a6c4ff36eb3329
royeradames <royeraadames@gmail.com>
2021-11-25 12:53:09 -0500

    📦resolve crewMember query

--- server/api/resolvers.ts
+++ server/api/resolvers.ts
@@ -2,6 +2,9 @@ const resolvers = {
   Query: {
     /* Find list all crew members */
     crew: (_, __, { dataSources }) => dataSources.db.getCrewMembers(),
+    /* get a crew member */
+    crewMember: (_, { position }, { dataSources }) =>
+      dataSources.db.getCrewMember(position),
   },
 };
 
