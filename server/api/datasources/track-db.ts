dc18df74e70b8df086532f26676ee7abc260223d
royeradames <royeraadames@gmail.com>
2021-11-25 13:58:20 -0500

    📦get all technologies from db

--- server/api/datasources/track-db.ts
+++ server/api/datasources/track-db.ts
@@ -6,13 +6,16 @@ class TrackDB extends SQLDataSource {
     return this.knex("crew").cache(MINUTE);
   }
   getCrewMember(position: string) {
-    return this.knex("crew").where({ position }).first().cache(MINUTE);
+    return this.knex("crew").where({ name: position }).first().cache(MINUTE);
   }
   getDestinations() {
     return this.knex("destination").cache(MINUTE);
   }
   getDestination(name: string) {
     return this.knex("destination").where({ name }).first().cache(MINUTE);
+  }
+  getTechnologies() {
+    return this.knex("technology").cache(MINUTE);
   }
 }
 
