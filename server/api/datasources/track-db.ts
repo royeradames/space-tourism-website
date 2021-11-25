095c8486a705b4cae16330a22a74fc0e0a5fd145
royeradames <royeraadames@gmail.com>
2021-11-25 12:53:50 -0500

    📦get a crewmember base on position

--- server/api/datasources/track-db.ts
+++ server/api/datasources/track-db.ts
@@ -5,6 +5,9 @@ class TrackDB extends SQLDataSource {
   getCrewMembers() {
     return this.knex("crew").cache(MINUTE);
   }
+  getCrewMember(position: string) {
+    return this.knex("crew").where({ position }).first().cache(MINUTE);
+  }
 }
 
 export default TrackDB;
