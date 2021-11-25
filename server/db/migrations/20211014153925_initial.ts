975f28e4ffe00dfb3f99dba2ff743421be523601
royeradames <royeraadames@gmail.com>
2021-11-24 19:35:56 -0500

    📦create destination table

--- server/db/migrations/20211014153925_initial.ts
+++ server/db/migrations/20211014153925_initial.ts
@@ -1,15 +1,24 @@
 import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable("crew", (table) => {
-    table.increments("crewId");
-    table.text("position");
-    table.text("name");
-    table.text("bio");
-    table.text("img");
-  });
+  return knex.schema
+    .createTable("crew", (table) => {
+      table.increments("crewId");
+      table.text("position");
+      table.text("name");
+      table.text("bio");
+      table.text("img");
+    })
+    .createTable("destination", (table) => {
+      table.increments("destinationId");
+      table.text("name");
+      table.text("message");
+      table.text("distance");
+      table.text("time");
+      table.text("img");
+    });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTable("crew");
+  return knex.schema.dropTable("crew").dropTable("destination");
 }
