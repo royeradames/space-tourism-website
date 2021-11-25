8dd80231f85de48a695907d3cac4f470395a2596
royeradames <royeraadames@gmail.com>
2021-11-25 13:57:14 -0500

    📦resolve destination query

--- server/api/resolvers.ts
+++ server/api/resolvers.ts
@@ -7,6 +7,9 @@ const resolvers = {
       dataSources.db.getCrewMember(position),
     /* Find list of all destination */
     destinations: (_, __, { dataSources }) => dataSources.db.getDestinations(),
+    /* get a destination information base on the name */
+    destination: (_, { planet }, { dataSources }) =>
+      dataSources.db.getDestination(planet),
   },
 };
 
