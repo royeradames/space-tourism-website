6fa4347a011c12df3b46a08eee8c7fa7bff4c092
royeradames <royeraadames@gmail.com>
2021-11-29 22:32:42 -0500

    📦add home to the / path

--- react-front-end/src/App.tsx
+++ react-front-end/src/App.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-// import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Header from "./component/header/header";
+import Home from "./component/home/home";
 function App() {
   return (
     <main>
       <Header />
       <Routes>
         {/* <Route>s renders the first one that matches the current URL. */}
+        <Route path="/" element={<Home />}></Route>
         {/* <Route path="/about"><About /></Route> */}
         {/* <Route path="/users"><Users /></Route> */}
-        {/* <Route path="/"><Home /></Route> */}
       </Routes>
     </main>
   );
