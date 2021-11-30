58ecd8e02b8062fbb58339ec79e71e672b9aec38
royeradames <royeraadames@gmail.com>
2021-11-30 01:50:09 -0500

    📦add destination route and dynamic class

--- react-front-end/src/App.tsx
+++ react-front-end/src/App.tsx
@@ -4,20 +4,21 @@ import "./App.scss";
 import Header from "./component/header/header";
 import Home from "./component/home/home";
 import Technology from "./component/technology/technology";
+import Destinataion from "./component/destination/destination";
 function App() {
   console.log(useMatch("/"));
   return (
     <main
       className={`${useMatch("/") ? "page-home" : ""} ${
         useMatch("/technology") ? "page-technology" : ""
-      }`}
+      } ${useMatch("/destination") ? "page-destination" : ""}`}
     >
       <Header />
       <Routes>
         {/* <Route>s renders the first one that matches the current URL. */}
         <Route path="/" element={<Home />}></Route>
         <Route path="technology" element={<Technology />}></Route>
-        {/* <Route path="/users"><Users /></Route> */}
+        <Route path="destination" element={<Destinataion />}></Route>
       </Routes>
     </main>
   );
