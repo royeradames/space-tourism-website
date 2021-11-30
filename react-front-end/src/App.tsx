321a16c6c3a60dbe4b1ed033ab97326f225387f5
royeradames <royeraadames@gmail.com>
2021-11-29 21:23:15 -0500

    📦add react router dom

--- react-front-end/src/App.tsx
+++ react-front-end/src/App.tsx
@@ -1,26 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Header from "./component/header/header";
 function App() {
   return (
-    <div className="App">
+    <main>
       <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        {/* <Route>s renders the first one that matches the current URL. */}
+        {/* <Route path="/about"><About /></Route> */}
+        {/* <Route path="/users"><Users /></Route> */}
+        {/* <Route path="/"><Home /></Route> */}
+      </Routes>
+    </main>
   );
 }
 
