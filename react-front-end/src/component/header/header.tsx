60454e1765d8ceb752e2cc6708717a2a0af20693
royeradames <royeraadames@gmail.com>
2021-11-29 21:24:30 -0500

    📦add header in angular html

--- react-front-end/src/component/header/header.tsx
+++ react-front-end/src/component/header/header.tsx
@@ -2,5 +2,17 @@ import React from "react";
 import styles from "./header.module.scss";
 
 export default function header() {
-  return <h1 className={styles.title}>header is working</h1>;
+  return <header class="header">
+  <a routerLink="/" class="header-logo"
+    ><img src="assets/shared/logo.svg" alt="Logo icon"
+  /></a>
+  <button class="nav-toggle" (click)="toggle = !toggle">Menu</button>
+  <nav class="nav" [ngClass]="{ 'nav-hide': !toggle }">
+    <button (click)="toggle = !toggle" class="nav-close">close</button>
+    <a routerLink="/"><span>00</span> HOME</a>
+    <a routerLink="/destination"><span>01</span> DESTINATION</a>
+    <a routerLink="/crew"><span>02</span> CREW</a>
+    <a routerLink="/technology"><span>03</span> TECHNOLOGY</a>
+  </nav>
+</header>
 }
