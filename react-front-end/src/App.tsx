fd4a05054d097e5f40bd338289d5a044c26bd6ab
royeradames <royeraadames@gmail.com>
2021-11-29 23:36:07 -0500

    📦add home page and techology background when on matching url

--- react-front-end/src/App.tsx
+++ react-front-end/src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useMatch } from "react-router-dom";
 import "./App.scss";
 import Header from "./component/header/header";
 import Home from "./component/home/home";
 import Technology from "./component/technology/technology";
 function App() {
+  console.log(useMatch("/"));
   return (
-    <main>
+    <main
+      className={`${useMatch("/") ? "page-home" : ""} ${
+        useMatch("/technology") ? "page-technology" : ""
+      }`}
+    >
       <Header />
       <Routes>
         {/* <Route>s renders the first one that matches the current URL. */}
