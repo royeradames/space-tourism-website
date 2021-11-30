2c34757a26761f3bb8ba5b4b90c59911e6d2d582
royeradames <royeraadames@gmail.com>
2021-11-30 15:49:12 -0500

    📦get page data from server instead of state

--- react-front-end/src/component/home/Home.tsx
+++ react-front-end/src/component/home/Home.tsx
@@ -1,26 +1,41 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./home.module.scss";
 import { Link } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
 
+/* query to get home page data */
+export const HOME_PAGE_QUERY = gql`
+  query getHomeData {
+    home {
+      title
+      titleSpan
+      message
+      action
+    }
+  }
+`;
+
+interface PageData {
+  title: string;
+  titleSpan: string;
+  message: string;
+  action: string;
+}
 export default function Home() {
-  const pageData = {
-    title: "SO, YOU WANT TO TRAVEL TO",
-    titleSpan: "SPACE",
-    message: `Let’s face it; if you want to go to space, you might as well genuinely go to
-    outer space and not hover kind of on the edge of it. Well sit back, and
-    relax because we’ll give you a truly out of this world experience!`,
-    action: "EXPLORE",
-  };
-  const [page] = useState(pageData);
+  const { loading, error, data } = useQuery(HOME_PAGE_QUERY);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  const { home }: { home: PageData } = data;
+
   return (
     <article className={styles.home}>
       <h1 className={styles["home-title"]}>
-        {page.title}
-        <span>{page.titleSpan}</span>
+        {home.title}
+        <span>{home.titleSpan}</span>
       </h1>
-      <p className={styles["home-message"]}>{page.message}</p>
+      <p className={styles["home-message"]}>{home.message}</p>
       <Link className={styles["home-action"]} to="/destination">
-        {page.action}
+        {home.action}
       </Link>
     </article>
   );
