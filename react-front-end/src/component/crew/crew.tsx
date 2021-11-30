f155b85cee670ee1b89fbe2cf7f0647f4a14330a
royeradames <royeraadames@gmail.com>
2021-11-30 13:47:09 -0500

    📦convert angular func to react

--- react-front-end/src/component/crew/crew.tsx
+++ react-front-end/src/component/crew/crew.tsx
@@ -8,46 +8,56 @@ export default function Crew() {
         bio: `Douglas Gerald Hurley is an American engineer, former Marine Corps pilot
     and former NASA astronaut. He launched into space for the third time as
     commander of Crew Dragon Demo-2.`,
-        img: 'assets/crew/image-douglas-hurley.webp',
+        img: '../../assets/crew/image-douglas-hurley.webp',
     }
     const [page, setPage] = useState(pageData)
     /* change template data base on list click */
-  getCrewInfo(crewMember: string) {
+  const getCrewInfo = (crewMember: string) => {
     switch (crewMember) {
       case 'commander':
-        this.position = 'COMMANDER';
-        this.name = 'Douglas Hurley';
-        this.bio = `Douglas Gerald Hurley is an American engineer, former Marine Corps pilot
+        setPage({
+            position: 'COMMANDER',
+            name: 'Douglas Hurley',
+            bio: `Douglas Gerald Hurley is an American engineer, former Marine Corps pilot
           and former NASA astronaut. He launched into space for the third time as
-          commander of Crew Dragon Demo-2.`;
-        this.img = 'assets/crew/image-douglas-hurley.webp';
+          commander of Crew Dragon Demo-2.`,
+            img: '../../assets/crew/image-douglas-hurley.webp',
+        })
         break;
       case 'specialist':
-        this.position = 'Mission Specialist';
-        this.name = 'MARK SHUTTLEWORTH';
-        this.bio = `Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.`;
-        this.img = 'assets/crew/image-mark-shuttleworth.webp';
+        setPage({
+            position: 'Mission Specialist',
+            name: 'MARK SHUTTLEWORTH',
+            bio: `Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.`,
+            img: '../../assets/crew/image-mark-shuttleworth.webp',
+        })
         break;
       case 'pilot':
-        this.position = 'PILOT';
-        this.name = 'Victor Glover';
-        this.bio = `Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.`;
-        this.img = 'assets/crew/image-victor-glover.webp';
+        setPage({
+            position: 'PILOT',
+            name: 'Victor Glover',
+            bio: `Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.`,
+            img: '../../assets/crew/image-victor-glover.webp',
+        })
         break;
       case 'engineer':
-        this.position = 'Flight Engineer';
-        this.name = 'Anousheh Ansari';
-        this.bio = `Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.`;
-        this.img = 'assets/crew/image-anousheh-ansari.webp';
+          setPage({
+            position: 'Flight Engineer',
+            name: 'Anousheh Ansari',
+            bio: `Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.`,
+            img: '../../assets/crew/image-anousheh-ansari.webp'
+          })
         break;
 
       default:
-        this.position = 'COMMANDER';
-        this.name = 'Douglas Hurley';
-        this.bio = `Douglas Gerald Hurley is an American engineer, former Marine Corps pilot
+        setPage({
+            position: 'COMMANDER',
+            name: 'Douglas Hurley',
+            bio: `Douglas Gerald Hurley is an American engineer, former Marine Corps pilot
           and former NASA astronaut. He launched into space for the third time as
-          commander of Crew Dragon Demo-2.`;
-        this.img = 'assets/crew/image-douglas-hurley.webp';
+          commander of Crew Dragon Demo-2.`,
+            img: '../../assets/crew/image-douglas-hurley.webp',
+        })
         break;
     }
   }
