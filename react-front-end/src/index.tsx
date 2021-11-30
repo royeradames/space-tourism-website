a629b6c0fd6e12bab5c6acd6877b31277b285dfa
royeradames <royeraadames@gmail.com>
2021-11-30 15:32:33 -0500

    📦setup apollo client

--- react-front-end/src/index.tsx
+++ react-front-end/src/index.tsx
@@ -4,12 +4,19 @@ import { BrowserRouter } from "react-router-dom";
 import "./sass/index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
