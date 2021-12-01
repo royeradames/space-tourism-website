e1c84ecf94c4fa3d24a9ce5a3032e817305df693
royeradames <royeraadames@gmail.com>
2021-11-30 23:41:24 -0500

    👌move test for another time

--- react-front-end/src/component/header/header.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import Header from "./header";
-
-it("renders welcome message", () => {
-  render(<Header />);
-  expect(screen.getByText(/Header is working/i)).toBeInTheDocument();
-});
