56d297f07792a5442d987e8f3e380f9568a05050
royeradames <royeraadames@gmail.com>
2021-11-30 21:24:38 -0500

    📦replace hard code data with graphql query

--- react-front-end/src/component/destination/destination.tsx
+++ react-front-end/src/component/destination/destination.tsx
@@ -1,145 +1,134 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./destination.module.scss";
+import { useQuery, gql } from "@apollo/client";
 /* images */
-import moom from "../../assets/destination/image-moon.webp";
-import mars from "../../assets/destination/image-mars.webp";
-import europa from "../../assets/destination/image-europa.webp";
-import titan from "../../assets/destination/image-titan.webp";
+import moonImg from "../../assets/destination/image-moon.webp";
+import marsImg from "../../assets/destination/image-mars.webp";
+import europaImg from "../../assets/destination/image-europa.webp";
+import titanImg from "../../assets/destination/image-titan.webp";
 
-export default function Destinatation() {
-  const destinationData = {
-    name: "MOON",
-    message: `See our planet as you’ve never seen it before. A perfect relaxing trip
-          away to help regain perspective and come back refreshed. While you’re
-          there, take in some history by visiting the Luna 2 and Apollo 11 landing
-          sites.`,
-    distance: "384,400 km",
-    time: "3 DAYS",
-    img: moom,
-  };
+interface DestinationData {
+  name: string;
+  message: string;
+  distance: string;
+  time: string;
+  img: string;
+}
 
-  const [destination, setDestination] = useState(destinationData);
-  /* change page data base on list selection */
-  const getPlanetInfo = (planetName: string) => {
-    switch (planetName) {
-      case "moon":
-        setDestination({
-          name: "MOON",
-          message: `See our planet as you’ve never seen it before. A perfect relaxing trip
-          away to help regain perspective and come back refreshed. While you’re
-          there, take in some history by visiting the Luna 2 and Apollo 11 landing
-          sites.`,
-          distance: "384,400 km",
-          time: "3 DAYS",
-          img: moom,
-        });
-
-        break;
-      case "mars":
-        setDestination({
-          name: "MARS",
-          message: `Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!`,
-          distance: "225 MIL. km",
-          time: "9 MONTHS",
-          img: mars,
-        });
-        break;
-      case "europa":
-        setDestination({
-          name: "EUROPA",
-          message: `The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.`,
-          distance: "628 MIL. km",
-          time: "3 YEARS",
-          img: europa,
-        });
-        break;
-      case "titan":
-        setDestination({
-          name: "Titan",
-          message: `The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.`,
-          distance: "1.6 BIL. km",
-          time: "7 YEARS",
-          img: titan,
-        });
-
-        break;
-
-      default:
-        setDestination({
-          name: "MOON",
-          message: `See our planet as you’ve never seen it before. A perfect relaxing trip
-          away to help regain perspective and come back refreshed. While you’re
-          there, take in some history by visiting the Luna 2 and Apollo 11 landing
-          sites.`,
-          distance: "384,400 km",
-          time: "3 DAYS",
-          img: "assets/destination/image-moon.webp",
-        });
-        break;
+export const GET_DESTINATION = gql`
+  query getDestinations {
+    destinations {
+      name
+      message
+      distance
+      time
     }
-  };
-
-  return (
-    <article className={styles["destination"]}>
-      <h1 className={styles["destination-title"]}>
-        <span>01</span> PICK YOUR DESTINATION
-      </h1>
-      <img
-        src={destination.img}
-        alt={destination.name}
-        className={styles["destination-img"]}
-      />
-
-      <nav className={styles["destination-list"]}>
-        <button
-          onClick={() => getPlanetInfo("moon")}
-          className={styles["planet-list-moon"]}
-        >
-          MOON
-        </button>
-        <button
-          onClick={() => getPlanetInfo("mars")}
-          className={styles["planet-list-mars"]}
-        >
-          MARS
-        </button>
-        <button
-          onClick={() => getPlanetInfo("europa")}
-          className={styles["planet-list-europa"]}
-        >
-          EUROPA
-        </button>
-        <button
-          onClick={() => getPlanetInfo("titan")}
-          className={styles["planet-list-titan"]}
-        >
-          TITAN
-        </button>
-      </nav>
-      <h2 className={styles["destination-name"]}>{destination.name}</h2>
-      <p className={styles["destination-message"]}>{destination.message}</p>
-
-      <hr className={styles["destination-line"]} />
-      <h2
-        className={`${styles["destination-label"]} ${styles["destination-label-distance"]}`}
-      >
-        AVG. DISTANCE
-      </h2>
-      <p
-        className={`${styles["destination-stats"]} ${styles["destination-stats-distance"]}`}
-      >
-        {destination.distance}
-      </p>
-      <h2
-        className={`${styles["destination-label"]} ${styles["destination-label-time"]}`}
-      >
-        Est. travel time
-      </h2>
-      <p
-        className={`${styles["destination-stats"]} ${styles["destination-stats-time"]}`}
-      >
-        {destination.time}
-      </p>
-    </article>
+  }
+`;
+export default function Destinatation() {
+  const [destination, setDestination] = useState<undefined | DestinationData>(
+    undefined
   );
+
+  const { loading, error, data } = useQuery(GET_DESTINATION);
+  useEffect(() => {
+    if (loading === false && data) {
+      // default to moon
+      let moon = data.destinations[0];
+      moon = {
+        ...moon,
+        img: moonImg,
+      };
+      setDestination(moon);
+    }
+  }, [loading, data]);
+
+  // handle error
+  if (error) return <div>Error: {error.message}</div>;
+  // handle the data
+  else if (destination) {
+    let [moon, mars, europa, titan] = data.destinations;
+    // add destination image
+    moon = {
+      ...moon,
+      img: moonImg,
+    };
+    mars = {
+      ...mars,
+      img: marsImg,
+    };
+    europa = {
+      ...europa,
+      img: europaImg,
+    };
+    titan = {
+      ...titan,
+      img: titanImg,
+    };
+    return (
+      <article className={styles["destination"]}>
+        <h1 className={styles["destination-title"]}>
+          <span>01</span> PICK YOUR DESTINATION
+        </h1>
+        <img
+          src={destination.img}
+          alt={destination.name}
+          className={styles["destination-img"]}
+        />
+
+        <nav className={styles["destination-list"]}>
+          <button
+            onClick={() => setDestination(moon)}
+            className={styles["planet-list-moon"]}
+          >
+            MOON
+          </button>
+          <button
+            onClick={() => setDestination(mars)}
+            className={styles["planet-list-mars"]}
+          >
+            MARS
+          </button>
+          <button
+            onClick={() => setDestination(europa)}
+            className={styles["planet-list-europa"]}
+          >
+            EUROPA
+          </button>
+          <button
+            onClick={() => setDestination(titan)}
+            className={styles["planet-list-titan"]}
+          >
+            TITAN
+          </button>
+        </nav>
+        <h2 className={styles["destination-name"]}>{destination.name}</h2>
+        <p className={styles["destination-message"]}>{destination.message}</p>
+
+        <hr className={styles["destination-line"]} />
+        <h2
+          className={`${styles["destination-label"]} ${styles["destination-label-distance"]}`}
+        >
+          AVG. DISTANCE
+        </h2>
+        <p
+          className={`${styles["destination-stats"]} ${styles["destination-stats-distance"]}`}
+        >
+          {destination.distance}
+        </p>
+        <h2
+          className={`${styles["destination-label"]} ${styles["destination-label-time"]}`}
+        >
+          Est. travel time
+        </h2>
+        <p
+          className={`${styles["destination-stats"]} ${styles["destination-stats-time"]}`}
+        >
+          {destination.time}
+        </p>
+      </article>
+    );
+  }
+  // handle loading
+  else return <div>Loading...</div>;
 }
