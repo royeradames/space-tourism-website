45588a359660756c31dd05bce8eb7898a709711c
royeradames <royeraadames@gmail.com>
2021-11-30 19:08:39 -0500

    👌update uri

--- react-front-end/src/index.tsx
+++ react-front-end/src/index.tsx
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: "http://localhost:4000/",
   cache: new InMemoryCache(),
 });
 ReactDOM.render(
